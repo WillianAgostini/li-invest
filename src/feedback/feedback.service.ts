da223719d8678e202d05f8fd83094593503fac3d
Willian Agostini <willian.agostini@gmail.com>
2024-06-12 17:46:38 -0300

    fix: create connection for insert

--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -7,24 +7,29 @@ dotenv.config();
 export class FeedbackService {
   private readonly logger = new Logger(FeedbackService.name);
 
-  private sql = postgres({
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    ssl: 'require',
-  });
+  getConnection() {
+    return postgres({
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      ssl: 'require',
+    });
+  }
 
   async insertData(jsonData: any) {
+    const sql = this.getConnection();
     try {
-      const result = await this.sql`
+      const result = await sql`
             INSERT INTO feedback (data)
-            VALUES (${this.sql.json(jsonData)})
+            VALUES (${sql.json(jsonData)})
             RETURNING id, created_at
           `;
       this.logger.debug('Data inserted:', result);
     } catch (error) {
       this.logger.error('Error inserting data:', error);
+    } finally {
+      sql.end();
     }
   }
 }
