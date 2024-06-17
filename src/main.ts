88e9bc72caa803776e41affabc4c0868534058d6
Willian Agostini <willian.agostini@gmail.com>
2024-06-17 13:56:22 -0300

    docs: endpoint for swagger

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { writeFileSync } from 'fs';
-import { SimulationService } from './simulation/simulation.service';
+// import { SimulationService } from './simulation/simulation.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,12 +13,13 @@ async function bootstrap() {
     .setTitle('Investment Advisor')
     .setDescription('Assists in choosing fixed-income investments: CDB, RDB, LCI, LCA, and Savings Account.')
     .setVersion('1.0')
-    .addServer('https://lb-node-willians-project-0e2f53cf.koyeb.app')
+    .addServer('https://li-invest.koyeb.app')
     .build();
 
   const document = { ...SwaggerModule.createDocument(app, config), openapi: '3.1.0' };
-  writeFileSync('./swagger-spec.json', JSON.stringify(document, null, 2));
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'swagger',
+  });
 
   // const simulationService = app.get<SimulationService>(SimulationService);
   // console.log(await simulationService.simulate({
