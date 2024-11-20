04110c9e1de78364a2867cfc7069ebbf88d2e498
Willian Agostini <willian.agostini@gmail.com>
2024-11-20 13:10:01 -0300

    fix: remove auth

--- src/decorator/skip-auth.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const IS_PUBLIC_KEY = 'skipAuth';
-export const SkipAuth = () => SetMetadata(IS_PUBLIC_KEY, true);
--- src/regulations/controllers/regulations.controller.ts
+++ src/regulations/controllers/regulations.controller.ts
@@ -2,19 +2,16 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import privacyPolicies from '../view/privacyPolicies';
 import termsAndConditions from '../view/termsAndConditions';
-import { SkipAuth } from 'src/decorator/skip-auth.decorator';
 
 @Controller('regulations')
 @ApiTags('regulations')
 export class RegulationsController {
-  @SkipAuth()
   @Get('privacyPolicies')
   @ApiResponse({ status: 200 })
   async getPrivacyPolicies() {
     return privacyPolicies;
   }
 
-  @SkipAuth()
   @Get('termsAndConditions')
   @ApiResponse({ status: 200 })
   async getTermsAndConditions() {
